f8e91d01a506afdfb6d9aa2d2135fee8240e7d17
bigfudge <bigfudge2123@gmail.com>
2021-07-07 14:58:03 +0800

    test

--- review/main.cpp
+++ review/main.cpp
@@ -21,7 +21,7 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
